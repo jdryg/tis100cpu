0be597739702cb4743c38ba90cca47584927af83
Jim Drygiannakis <jdryg@users.noreply.github.com>
2015-07-07 17:32:36 +0300

    Instruction decoder testbench

--- instruction_decoder.vhd
+++ instruction_decoder.vhd
@@ -7,7 +7,7 @@ entity instruction_decoder is
          O_srcA : out  STD_LOGIC_VECTOR (2 downto 0);
          O_srcB : out  STD_LOGIC_VECTOR (1 downto 0);
          O_imm : out  STD_LOGIC_VECTOR (15 downto 0);
-         O_aluOp: out STD_LOGIC_VECTOR (2 downto 0);
+         O_aluOp: out STD_LOGIC_VECTOR (1 downto 0);
          O_srcA_isPort : out  STD_LOGIC;
          O_dst_isPort : out  STD_LOGIC;
          O_enableWrite : out  STD_LOGIC;
@@ -19,11 +19,11 @@ end instruction_decoder;
 architecture Behavioral of instruction_decoder is
 begin
   decode_proc: process (I_instr) begin
-    O_dst <= I_instr(8 downto 6);
-    O_srcA <= I_instr(11 downto 9);
-    O_srcB <= I_instr(13 downto 12);
-    O_imm <= I_instr(31 downto 16);
-    O_containsIMM <= I_instr(0);
+    O_containsIMM <= I_instr(31);
+    O_dst <= I_instr(25 downto 23);
+    O_srcA <= I_instr(22 downto 20);
+    O_srcB <= I_instr(19 downto 18);
+    O_imm <= I_instr(15 downto 0);   
 
     -- Default values for the rest of the signals
     O_aluOp <= "00";
@@ -34,30 +34,30 @@ begin
     O_jmpCondition <= "111";
 
     -- Depending on the instruction type, set the correct outputs.
-    if (I_instr(2 downto 1) = "00") then
+    if (I_instr(30 downto 29) = "00") then
       -- Arithmetic instructions
       O_enableWrite <= '1';
-      case I_instr(5 downto 3) is 
+      case I_instr(28 downto 26) is 
         when "000" => O_aluOp <= "00";
         when "001" => O_aluOp <= "01";
         when "010" => O_aluOp <= "10";
         when others => O_aluOp <= "00";
       end case;
       -- The rest of the outputs get their default value.
-    elsif (I_instr (2 downto 1) = "01") then
+    elsif (I_instr (30 downto 29) = "01") then
       -- Port instruction
       O_enableWrite <= '1';
-      if (I_instr(5 downto 3) = "000") then
+      if (I_instr(28 downto 26) = "000") then
         -- RDP
         O_srcA_isPort <= '1';
-      elsif (I_instr(5 downto 3) = "001") then
+      elsif (I_instr(28 downto 26) = "001") then
         -- WRP
         O_dst_isPort <= '1';
       end if;
-    elsif (I_instr (2 downto 1) = "10") then
+    elsif (I_instr (30 downto 29) = "10") then
       -- Jxx instruction
       O_isJmp <= '1';
-      O_jmpCondition <= I_instr(5 downto 3);
+      O_jmpCondition <= I_instr(28 downto 26);
       O_aluOp <= "11"; -- Set Less Than
     end if;
   end process;
---
+++ instruction_decoder_tb.vhd
@@ -0,0 +1,110 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+ENTITY instruction_decoder_tb IS
+END instruction_decoder_tb;
+ 
+ARCHITECTURE behavior OF instruction_decoder_tb IS 
+  -- Component Declaration for the Unit Under Test (UUT)
+ 
+  COMPONENT instruction_decoder
+    PORT(
+      I_instr        : IN  std_logic_vector(31 downto 0);
+      O_dst          : OUT  std_logic_vector(2 downto 0);
+      O_srcA         : OUT  std_logic_vector(2 downto 0);
+      O_srcB         : OUT  std_logic_vector(1 downto 0);
+      O_imm          : OUT  std_logic_vector(15 downto 0);
+      O_aluOp        : OUT  std_logic_vector(1 downto 0);
+      O_srcA_isPort  : OUT  std_logic;
+      O_dst_isPort   : OUT  std_logic;
+      O_enableWrite  : OUT  std_logic;
+      O_containsIMM  : OUT  std_logic;
+      O_isJmp        : OUT  std_logic;
+      O_jmpCondition : OUT  std_logic_vector(2 downto 0)
+    );
+  END COMPONENT;
+
+  --Inputs
+  signal I_instr : std_logic_vector(31 downto 0) := (others => '0');
+
+ 	--Outputs
+  signal O_dst : std_logic_vector(2 downto 0);
+  signal O_srcA : std_logic_vector(2 downto 0);
+  signal O_srcB : std_logic_vector(1 downto 0);
+  signal O_imm : std_logic_vector(15 downto 0);
+  signal O_aluOp : std_logic_vector(1 downto 0);
+  signal O_srcA_isPort : std_logic;
+  signal O_dst_isPort : std_logic;
+  signal O_enableWrite : std_logic;
+  signal O_containsIMM : std_logic;
+  signal O_isJmp : std_logic;
+  signal O_jmpCondition : std_logic_vector(2 downto 0);
+
+BEGIN
+	-- Instantiate the Unit Under Test (UUT)
+  uut: instruction_decoder PORT MAP (
+    I_instr => I_instr,
+    O_dst => O_dst,
+    O_srcA => O_srcA,
+    O_srcB => O_srcB,
+    O_imm => O_imm,
+    O_aluOp => O_aluOp,
+    O_srcA_isPort => O_srcA_isPort,
+    O_dst_isPort => O_dst_isPort,
+    O_enableWrite => O_enableWrite,
+    O_containsIMM => O_containsIMM,
+    O_isJmp => O_isJmp,
+    O_jmpCondition => O_jmpCondition
+  );
+
+  -- Stimulus process
+  stim_proc: process
+  begin
+    I_instr <= X"80800005";
+    wait for 10 ns;
+    assert O_dst = "001"          report "(1) Invalid dst value" severity error;
+    assert O_srcA = "000"         report "(1) Invalid srcA value" severity error;
+    assert O_srcB = "00"          report "(1) Invalid srcB value" severity error;
+    assert O_imm = X"0005"        report "(1) Invalid immediate operand value" severity error;
+    assert O_aluOp = "00"         report "(1) Invalid ALU operation" severity error;
+    assert O_srcA_isPort = '0'    report "(1) Invalid srcA_isPort flag" severity error;
+    assert O_dst_isPort = '0'     report "(1) Invalid dst_isPort flag" severity error;
+    assert O_enableWrite = '1'    report "(1) Invalid enableWrite flag" severity error;
+    assert O_containsIMM = '1'    report "(1) Invalid containsIMM flag" severity error;
+    assert O_isJmp = '0'          report "(1) Invalid isJmp flag" severity error;
+    assert O_jmpCondition = "111" report "(1) Invalid jump condition" severity error;
+    report "Finished decoding 'ADD ACC, NIL, 5'";
+    
+    I_instr <= X"84900001";
+    wait for 10 ns;
+    assert O_dst = "001"          report "(2) Invalid dst value" severity error;
+    assert O_srcA = "001"         report "(2) Invalid srcA value" severity error;
+    assert O_srcB = "00"          report "(2) Invalid srcB value" severity error;
+    assert O_imm = X"0001"        report "(2) Invalid immediate operand value" severity error;
+    assert O_aluOp = "01"         report "(2) Invalid ALU operation" severity error;
+    assert O_srcA_isPort = '0'    report "(2) Invalid srcA_isPort flag" severity error;
+    assert O_dst_isPort = '0'     report "(2) Invalid dst_isPort flag" severity error;
+    assert O_enableWrite = '1'    report "(2) Invalid enableWrite flag" severity error;
+    assert O_containsIMM = '1'    report "(2) Invalid containsIMM flag" severity error;
+    assert O_isJmp = '0'          report "(2) Invalid isJmp flag" severity error;
+    assert O_jmpCondition = "111" report "(2) Invalid jump condition" severity error;
+    report "Finished decoding 'SUB ACC, ACC, 1'";
+
+    I_instr <= X"CC100005";
+    wait for 10 ns;
+    assert O_dst = "000"          report "(3) Invalid dst value" severity error;
+    assert O_srcA = "001"         report "(3) Invalid srcA value" severity error;
+    assert O_srcB = "00"          report "(3) Invalid srcB value" severity error;
+    assert O_imm = X"0005"        report "(3) Invalid immediate operand value" severity error;
+    assert O_aluOp = "11"         report "(3) Invalid ALU operation" severity error;
+    assert O_srcA_isPort = '0'    report "(3) Invalid srcA_isPort flag" severity error;
+    assert O_dst_isPort = '0'     report "(3) Invalid dst_isPort flag" severity error;
+    assert O_enableWrite = '0'    report "(3) Invalid enableWrite flag" severity error;
+    assert O_containsIMM = '1'    report "(3) Invalid containsIMM flag" severity error;
+    assert O_isJmp = '1'          report "(3) Invalid isJmp flag" severity error;
+    assert O_jmpCondition = "011" report "(3) Invalid jump condition" severity error;
+    report "Finished decoding 'JMP EQUAL, 5'";
+    
+    wait;
+  end process;
+END;
